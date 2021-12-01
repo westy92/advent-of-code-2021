c72341d67f7014eaccdabaececf3a2a62e79a213
Seth Westphal <westy92@gmail.com>
2021-12-01 00:33:10 -0600

    Complete day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(lines: List<String>): Int {
+        var increases = 0
+        var previous = lines.firstOrNull()?.toIntOrNull() ?: return 0
+        for (line in lines.drop(1)) {
+            val current = line.toIntOrNull() ?: return increases
+            if (current > previous) increases++
+            previous = current
+        }
+        return increases
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(lines: List<String>): Int {
+        var increases = 0
+        val intLines = lines.mapNotNull { it.toIntOrNull() }
+        var group = intLines.take(3).toMutableList()
+        var previous = group.sum()
+        for (line in intLines.drop(3)) {
+            group.removeFirst()
+            group.add(line)
+            val current = group.sum()
+            if (current > previous) increases++
+            previous = current
+        }
+        return increases
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val lines = readInput("Day01")
+    check(part1(testInput) == 7)
+    println(part1(lines))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    check(part2(testInput) == 5)
+    println(part2(lines))
 }
